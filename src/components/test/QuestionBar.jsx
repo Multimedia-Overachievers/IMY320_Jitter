a84176686ca99d666c36a99721c53aed19445c6b
Keelan <keelanmatthews123@gmail.com>
2023-09-06 17:57:12 +0200

    fixed key issue

--- src/components/test/QuestionBar.jsx
+++ src/components/test/QuestionBar.jsx
@@ -5,7 +5,7 @@ export default function QuestionBar({ questionData }) {
         <>
             <div className='d-flex justify-content-between w-100 mt-5'>
                 {questionData?.map((question, index) => (
-                    <div key={index} className={`position-relative rounded-1 p-2 mx-1 bg-${question.completed ? 'primary' : 'secondary'} flex-grow-1`} key={index}>
+                    <div key={index} className={`position-relative rounded-1 p-2 mx-1 bg-${question.completed ? 'primary' : 'secondary'} flex-grow-1`}>
                         {question.active && <div className="triangle"></div>}
                     </div>
                 ))}
