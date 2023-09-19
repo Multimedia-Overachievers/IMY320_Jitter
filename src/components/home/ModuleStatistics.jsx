f2dbdcbd3cea28060c25d1ee146da95e15cc7b5f
Francois <contact@francois-smith.com>
2023-09-19 16:31:58 +0200

    Linked Scores To Chart

--- src/components/home/ModuleStatistics.jsx
+++ src/components/home/ModuleStatistics.jsx
@@ -12,39 +12,6 @@ import { BarChart } from "./BarChart";
 
 import questions from '../../backend/json/questions.json';
 
-export const Data = [
-    {
-      id: 1,
-      year: 2016,
-      userGain: 80000,
-      userLost: 823
-    },
-    {
-      id: 2,
-      year: 2017,
-      userGain: 45677,
-      userLost: 345
-    },
-    {
-      id: 3,
-      year: 2018,
-      userGain: 78888,
-      userLost: 555
-    },
-    {
-      id: 4,
-      year: 2019,
-      userGain: 90000,
-      userLost: 4555
-    },
-    {
-      id: 5,
-      year: 2020,
-      userGain: 4300,
-      userLost: 234
-    }
-  ];
-
 Chart.register(CategoryScale);
 Chart.register(ChartDataLabels);
 
@@ -70,7 +37,7 @@ export default function ModuleStatistics({ module, timeSpent }) {
     datasets: [
                 {
                     label: 'Average Score',
-                    data: [20, 33, 23, 42],
+                    data: module?.chapters.map(chapter => getAverage(chapter.scores)),
                     fill: false,
                     backgroundColor: '#6299EB',
                     borderColor: '#6299EB',
