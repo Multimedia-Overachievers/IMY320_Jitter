e1dbf326b31ccc8c1d98ae3f11fa33989e581639
Francois <contact@francois-smith.com>
2023-09-22 21:14:30 +0200

    Updated endpoint

--- src/services/api-url.jsx
+++ src/services/api-url.jsx
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'http://localhost:5000',
-    //baseURL: 'https://quilled-viridian-tempo.glitch.me',
+    //baseURL: 'http://localhost:5000',
+    baseURL: 'https://jitter-backend.onrender.com',
     headers: {
         'Content-Type': 'application/json',
         'Access-Control-Allow-Origin': '*'
