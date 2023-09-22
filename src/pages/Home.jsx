fbe76eff61c7fc64a3b6ac75307191618667c597
Keelan <keelanmatthews123@gmail.com>
2023-09-22 16:00:41 +0200

    Added Spinners to ModuleStatistics

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Row, Col } from 'react-bootstrap';
+import { Row, Col, Spinner } from 'react-bootstrap';
 import SideBar from '../components/sidebar/SideBar';
 import ModuleStatistics from '../components/home/ModuleStatistics';
 import Chapters from '../components/home/Chapters';
@@ -14,7 +14,7 @@ export default function Home() {
     const [activeModuleIndex, setActiveModuleIndex] = useState(0);
 
     useEffect(() => {
-        if(!modules){
+        if (!modules) {
             GetAllModules().then((response) => {
                 var modules = response.data;
                 setModules(modules);
@@ -38,58 +38,63 @@ export default function Home() {
 
     return (
         <div className="vh-100">
-            <Row>
-                <Col lg={9} className='scrollable vh-100'>
-                    {/* Background image */}
-                    <div className='bg-light position-absolute w-100 h-100 d-flex align-items-center justify-content-center' style={{zIndex: -1}}>
-                        <img className='background-image' src="/images/home-background.svg" alt="" />
-                    </div>
-                    <div className="p-5 m-4 " style={{zIndex: 2}}>
-                        {/* Home page header */}
-                        <div className='d-flex justify-content-between'>
-                            <div>
-                                <motion.h4 
-                                    className='text-secondary'
-                                    variants={slideInLeft}
-                                    initial='hidden'
-                                    animate='visible'
-                                    transition={{...transition, delay: 0.2}}
-                                >
-                                    Dashboard
-                                </motion.h4>
-                                <motion.h1 
-                                    className='text-primary fw-bold display-4'
-                                    variants={slideInLeft}
-                                    initial='hidden'
-                                    animate='visible'
-                                    transition={{...transition, delay: 0.5}}
-                                >
-                                    {module?.name}
-                                </motion.h1>
+            {
+                !modules ?
+                    <Spinner animation="border" variant="primary" className='position-absolute top-50 start-50 translate-middle' />
+                    :
+                    <Row>
+                        <Col lg={9} className='scrollable vh-100'>
+                            {/* Background image */}
+                            <div className='bg-light position-absolute w-100 h-100 d-flex align-items-center justify-content-center' style={{ zIndex: -1 }}>
+                                <img className='background-image' src="/images/home-background.svg" alt="" />
                             </div>
-                            {/*  Calculate and display the current date */}
-                            <motion.h4 
-                                className='text-secondary'
-                                variants={slideInRight}
-                                initial='hidden'
-                                animate='visible'
-                                transition={{...transition, delay: 0.5}}
-                            >
-                                {new Date().toLocaleDateString('en-ZA', { year: 'numeric', month: 'long', day: 'numeric' })}
-                            </motion.h4>
-                        </div>
-                        {/* Statistics section */}
-                        <ModuleStatistics module={module} />
-                        {/* Chapters section */}
-                        <Chapters module={module} />
-                    </div>
-                </Col>
+                            <div className="p-5 m-4 " style={{ zIndex: 2 }}>
+                                {/* Home page header */}
+                                <div className='d-flex justify-content-between'>
+                                    <div>
+                                        <motion.h4
+                                            className='text-secondary'
+                                            variants={slideInLeft}
+                                            initial='hidden'
+                                            animate='visible'
+                                            transition={{ ...transition, delay: 0.2 }}
+                                        >
+                                            Dashboard
+                                        </motion.h4>
+                                        <motion.h1
+                                            className='text-primary fw-bold display-4'
+                                            variants={slideInLeft}
+                                            initial='hidden'
+                                            animate='visible'
+                                            transition={{ ...transition, delay: 0.5 }}
+                                        >
+                                            {module?.name}
+                                        </motion.h1>
+                                    </div>
+                                    {/*  Calculate and display the current date */}
+                                    <motion.h4
+                                        className='text-secondary'
+                                        variants={slideInRight}
+                                        initial='hidden'
+                                        animate='visible'
+                                        transition={{ ...transition, delay: 0.5 }}
+                                    >
+                                        {new Date().toLocaleDateString('en-ZA', { year: 'numeric', month: 'long', day: 'numeric' })}
+                                    </motion.h4>
+                                </div>
+                                {/* Statistics section */}
+                                <ModuleStatistics module={module} />
+                                {/* Chapters section */}
+                                <Chapters module={module} />
+                            </div>
+                        </Col>
 
-                <Col lg={3}>
-                    {/* Side bar */}
-                    <SideBar modules={modules?.data} setModule={SetModuleCallback} />
-                </Col>
-            </Row>
+                        <Col lg={3}>
+                            {/* Side bar */}
+                            <SideBar modules={modules?.data} setModule={SetModuleCallback} />
+                        </Col>
+                    </Row>
+            }
         </div>
     )
 }
